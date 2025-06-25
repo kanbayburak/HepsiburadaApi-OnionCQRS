72541bf0cb6b2763949ab62939599c9991153f8e
Burak Kanbay <98902425+kanbayburak@users.noreply.github.com>
2025-06-26 00:28:30 +0300

    created Repositories

---
+++ Core/HepsiburadaApi.Application/Interfaces/IReadRepository.cs
@@ -0,0 +1,33 @@
+﻿using HepsiburadaApi.Domain.Common;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiburadaApi.Application.Interfaces
+{
+    public interface IReadRepository<T> where T : class, IEntityBase , new()
+    {
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null, //predicate, filtreleme şartlarını tanımlayan ve veritabanından hangi verilerin çekileceğini belirleyen bir koşuldur
+            Func<IQueryable<T>,IIncludableQueryable<T, object>>? include = null,  //bunun sayesinde GetAllAsync(x=>x.include(x=>x.brand).theninclude) şeklinde kyllanabiliyorum
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, //sıralama yapılması için
+            bool enableTracking = false);
+
+        Task<IList<T>> GetAllByPagingAsync(Expression<Func<T, bool>>? predicate = null, 
+            Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,  
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, 
+            bool enableTracking = false, int currentPage = 1, int pageSize = 3);
+
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate,  // burada predicate i zorunlu tutmamızın nedeni; bana sadece 1 tane veri dönecek ve o veriyi dönebilmemiz için buna kesinlikle predicate zorunlu tutmamız lazım
+           Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+           bool enableTracking = false);
+
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate, bool enableTracking = false);   //IQueryable, veritabanından veri çekerken "ne çekileceğini" tarif eder, ama hemen çalışmaz. Verileri gerçekten almak istediğinde (ToList(), FirstOrDefault() gibi) çalışır ve veritabanına sorgu gönderilir.
+
+
+        Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);  // CountAsync, veritabanındaki kayıtların sayısını verir.
+    }
+}
---
+++ Infrastructure/HepsiburadaApi.Persistance/Repositories/ReadRepository.cs
@@ -0,0 +1,71 @@
+﻿using HepsiburadaApi.Application.Interfaces;
+using HepsiburadaApi.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiburadaApi.Persistance.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly DbContext dbContext;
+        public ReadRepository(DbContext dbContext)       //DbContext imi ReadRepostory e entegre ediyorum
+        {
+            this.dbContext = dbContext;
+        }
+
+        private DbSet<T> Table { get => dbContext.Set<T>(); }  //normalde aşağıda her bir metod repository için " dbContext.Set<T>() " yapacağıma burada metod yaptık
+
+
+        public async Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool enableTracking = false)
+        {
+            IQueryable<T> queryable = Table; //dbSet ettiğim benim T nesnem bir Querybable olarak işlev görecektir
+            if (!enableTracking) queryable = queryable.AsNoTracking();  // tracking mekenizması sayesinde, sorgusunu yaptığımz işlmin üzerinde bir değişiklik yapıp yapmadığımızı işliyor, ama burada sadece read işlemi yapacağımız için için bunu daha performanslı olması adına devre dışı bırakmam gerekiyor
+            if (include is not null) queryable = include(queryable);
+            if(predicate is not null) queryable = queryable.Where(predicate);
+            if(orderBy is not null)
+                return await orderBy(queryable).ToListAsync();
+            return await queryable.ToListAsync();
+        }
+
+        public async Task<IList<T>> GetAllByPagingAsync(Expression<Func<T, bool>>? predicate = null, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, bool enableTracking = false, int currentPage = 1, int pageSize = 3)
+        {
+            IQueryable<T> queryable = Table; 
+            if (!enableTracking) queryable = queryable.AsNoTracking();  
+            if (include is not null) queryable = include(queryable);
+            if (predicate is not null) queryable = queryable.Where(predicate);
+            if (orderBy is not null)
+                return await orderBy(queryable).Skip((currentPage-1)*pageSize).Take(pageSize).ToListAsync();
+            return await queryable.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null, bool enableTracking = false)
+        {
+            IQueryable<T> queryable = Table;
+            if (!enableTracking) queryable = queryable.AsNoTracking();
+            if (include is not null) queryable = include(queryable);
+
+            return await queryable.FirstOrDefaultAsync(predicate);
+        }
+
+        public async Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null)
+        {
+            Table.AsNoTracking();   //Entity Framework'te AsNoTracking() demek: "Bu verileri getir ama takip etme, değişiklikleri izleme" anlamına gelir.
+            if(predicate is not null) Table.Where(predicate);
+
+            return await Table.CountAsync();
+        }
+
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate, bool enableTracking = false)
+        {
+            if (!enableTracking) Table.AsNoTracking();
+            return Table.Where(predicate);
+        }
+    }
+       
+}
