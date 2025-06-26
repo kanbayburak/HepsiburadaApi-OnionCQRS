e775de2e1c6b76f27fd88a3536f14f33ee351173
Burak Kanbay <98902425+kanbayburak@users.noreply.github.com>
2025-06-26 16:37:28 +0300

    create AutoMapper

--- Core/HepsiburadaApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
+++ Core/HepsiburadaApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -1,6 +1,8 @@
-﻿using HepsiburadaApi.Application.Interfaces.UnitOfWorks;
+﻿using HepsiburadaApi.Application.Interfaces.AutoMapper;
+using HepsiburadaApi.Application.Interfaces.UnitOfWorks;
 using HepsiburadaApi.Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,28 +14,23 @@ namespace HepsiburadaApi.Application.Features.Products.Queries.GetAllProducts
     public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQueryRequest, IList<GetAllProductsQueryResponse>>
     {
         private readonly IUnitOfWork unitOfWork;
-        public GetAllProductsQueryHandler(IUnitOfWork unitOfWork)
+        private readonly IMapper mapper;
+
+        public GetAllProductsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
         {
             this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
         }
 
         public async Task<IList<GetAllProductsQueryResponse>> Handle(GetAllProductsQueryRequest request, CancellationToken cancellationToken)
         {
-            var products = await unitOfWork.GetReadRepository<Product>().GetAllAsync();
+            var products = await unitOfWork.GetReadRepository<Product>().GetAllAsync(include: x =>x.Include(b => b.Brand));
 
-            List<GetAllProductsQueryResponse> response = new();
+            var map = mapper.Map<GetAllProductsQueryResponse, Product>(products);
 
-            foreach (var product in products)
-            {
-                response.Add(new GetAllProductsQueryResponse
-                {
-                    Title = product.Title,
-                    Description = product.Description,
-                    Discount = product.Discount,
-                    Price = product.Price - (product.Price * product.Discount / 100),
-                });
-            }
-            return response;
+            foreach (var item in map)
+                item.Price -= (item.Price * item.Discount / 100);
+            return map;
         }
     }
 }
--- Core/HepsiburadaApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryResponse.cs
+++ Core/HepsiburadaApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryResponse.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HepsiburadaApi.Application.DTOs;
 
 namespace HepsiburadaApi.Application.Features.Products.Queries.GetAllProducts
 {
@@ -12,5 +8,6 @@ namespace HepsiburadaApi.Application.Features.Products.Queries.GetAllProducts
         public string Description { get; set; }
         public decimal Price { get; set; }
         public decimal Discount { get; set; }
+        public BrandDto Brand { get; set; }
     }
 }
