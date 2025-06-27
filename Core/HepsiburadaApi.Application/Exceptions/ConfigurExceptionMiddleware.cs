f7c9386a9abe2acbd86ac58f47a25b178b55eedb
Burak Kanbay <98902425+kanbayburak@users.noreply.github.com>
2025-06-27 15:58:19 +0300

    created Globel Exceptions

---
+++ Core/HepsiburadaApi.Application/Exceptions/ConfigurExceptionMiddleware.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace HepsiburadaApi.Application.Exceptions
+{
+    public static class ConfigurExceptionMiddleware
+    {
+        public static void ConfigurExceptionHandlingMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
