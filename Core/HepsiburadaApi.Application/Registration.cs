3f2d402edd49d2c157d4a3f5fcb91f80cd280f3f
Burak Kanbay <98902425+kanbayburak@users.noreply.github.com>
2025-07-01 22:00:13 +0300

    creaated BaseRules structure

--- Core/HepsiburadaApi.Application/Registration.cs
+++ Core/HepsiburadaApi.Application/Registration.cs
@@ -5,6 +5,8 @@ using FluentValidation;
 using System.Globalization;
 using MediatR;
 using HepsiburadaApi.Application.Behaviors;
+using HepsiburadaApi.Application.Features.Products.Rules;
+using HepsiburadaApi.Application.Bases;
 
 
 namespace HepsiburadaApi.Application
@@ -17,12 +19,26 @@ namespace HepsiburadaApi.Application
 
             services.AddTransient<ExceptionMiddleware>();
 
+            services.AddRulesFromAssemblyContaining(assembly, typeof(BaseRules));
+
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
 
             services.AddValidatorsFromAssembly(assembly);
             ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("tr"); 
 
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(FluentValidationBehavior<,>));
+        }
+
+        //tüm Rules ları teker teker buraya bulup kaydetmektense BaseRules dan türemiş olanları bulup kaydetmesini sağladık
+        private static IServiceCollection AddRulesFromAssemblyContaining(
+            this IServiceCollection services,
+            Assembly assembly,
+            Type type) 
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+                services.AddTransient(item);
+                return services;
         }
     }
 }
