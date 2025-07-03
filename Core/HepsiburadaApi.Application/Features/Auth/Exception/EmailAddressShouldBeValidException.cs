0a4a70a4f6841026d12d9c19027ef63b8604b250
Burak Kanbay <98902425+kanbayburak@users.noreply.github.com>
2025-07-03 23:35:13 +0300

    Pratikte Revoke ve RevokeAll Yapısı

---
+++ Core/HepsiburadaApi.Application/Features/Auth/Exception/EmailAddressShouldBeValidException.cs
@@ -0,0 +1,9 @@
+﻿using HepsiburadaApi.Application.Bases;
+
+namespace HepsiburadaApi.Application.Features.Auth.Exception
+{
+    public class EmailAddressShouldBeValidException : BaseException
+    {
+        public EmailAddressShouldBeValidException() : base("Kullanıcı adı veya şifre yablıştır.") { }
+    }
+}
