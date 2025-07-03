80639386e232b7fb332a0e32b6a697613e8a41b1
Burak Kanbay <98902425+kanbayburak@users.noreply.github.com>
2025-07-03 12:07:43 +0300

    completed Register

---
+++ Core/HepsiburadaApi.Application/Features/Auth/Rules/AuthRules.cs
@@ -0,0 +1,20 @@
+﻿using HepsiburadaApi.Application.Bases;
+using HepsiburadaApi.Application.Features.Auth.Exception;
+using HepsiburadaApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiburadaApi.Application.Features.Auth.Rules
+{
+    public class AuthRules : BaseRules
+    {
+        public Task UserShouldNotBeExist(User? user)
+        {
+            if (user is not null) throw new UserAlreadyExistException();
+            return Task.CompletedTask;
+        }
+    }
+}
--- Core/HepsiburadaApi.Application/Features/Products/Command/UpdateProduct/UpdateProductCommandHandler.cs
+++ Core/HepsiburadaApi.Application/Features/Products/Command/UpdateProduct/UpdateProductCommandHandler.cs
@@ -1,7 +1,9 @@
-﻿using HepsiburadaApi.Application.Interfaces.AutoMapper;
+﻿using HepsiburadaApi.Application.Bases;
+using HepsiburadaApi.Application.Interfaces.AutoMapper;
 using HepsiburadaApi.Application.Interfaces.UnitOfWorks;
 using HepsiburadaApi.Domain.Entities;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +12,11 @@ using System.Threading.Tasks;
 
 namespace HepsiburadaApi.Application.Features.Products.Command.UpdateProduct
 {
-    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommandRequest, Unit>
+    public class UpdateProductCommandHandler : BaseHandler, IRequestHandler<UpdateProductCommandRequest, Unit>
     {
-        private readonly IUnitOfWork unitOfWork;
-        private readonly IMapper mapper;
-
-        public UpdateProductCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        public UpdateProductCommandHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
         {
-            this.unitOfWork = unitOfWork;
-            this.mapper = mapper;
+
         }
         public async Task<Unit> Handle(UpdateProductCommandRequest request, CancellationToken cancellationToken)
         {
